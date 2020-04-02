35ff33f28b8dfcce637fc8792bd2327d5a01aeed
Caio <caioymc@live.com>
2020-04-02 13:48:15 +0100

    Updates

---
+++ precache-manifest.11aca84b1911aa91521ef281450f8102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900c163c0bc1fdd20cd5293ba52e2d16",
+    "url": "/task-time-manager/index.html"
+  },
+  {
+    "revision": "ef5c58f1a62f03f6127d",
+    "url": "/task-time-manager/static/css/main.b38443b8.chunk.css"
+  },
+  {
+    "revision": "afb582862761b9ca92a8",
+    "url": "/task-time-manager/static/js/2.75c777b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-time-manager/static/js/2.75c777b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5c58f1a62f03f6127d",
+    "url": "/task-time-manager/static/js/main.117c15c6.chunk.js"
+  },
+  {
+    "revision": "e219e446dc78d4ea34d8",
+    "url": "/task-time-manager/static/js/runtime-main.40b20218.js"
+  },
+  {
+    "revision": "f5e4f17c1f6e93d299fd91d8b7dcc9bd",
+    "url": "/task-time-manager/static/media/header.f5e4f17c.jpeg"
+  }
+]);
